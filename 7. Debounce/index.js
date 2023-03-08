34a78a54673785b658ee0940ae0d0459ba3011b9
Ankur Sarkar <sarkarrohonankur@gmail.com>
2023-03-08 18:28:41 +0530

    update

--- 7. Debounce/index.js
+++ 7. Debounce/index.js
@@ -5,12 +5,21 @@
  * @return {Function}
  */
 export default function debounce(func, wait) {
-    let timeoutId
-    return function(...args){
-      // const context = this
-      clearTimeout(timeoutId)
-      timeoutId = setTimeout(()=>{
-        func.apply(this, args)
-      },wait)
-    }
+  let timeoutId;
+  function debouncedFunction(...args) {
+    // const context = this
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, wait);
   }
+
+  debouncedFunction.clear = function () {
+    clearTimeout(timeoutId);
+  };
+  debouncedFunction.flush = function (...args) {
+    clearTimeout(timeoutId);
+    func.apply(this, args);
+  };
+  return debouncedFunction;
+}
