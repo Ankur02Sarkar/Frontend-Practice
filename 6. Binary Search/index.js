485bd4570dfac1de24215f6d439af964cfe8c0fd
Ankur Sarkar <sarkarrohonankur@gmail.com>
2023-03-08 18:16:41 +0530

    update

--- 6. Binary Search/index.js
+++ 6. Binary Search/index.js
@@ -1,1 +1,23 @@
-// This is the index.js file for 6. Binary Search
+/**
+ * @param {Array<number>} arr The input integer array to be searched.
+ * @param {number} target Target integer to search within the array
+ * @return {Array<number>}
+ */
+export default function binarySearch(arr, target) {
+  let low = 0
+  let high = arr.length - 1
+  while(low<=high){
+    let mid = Math.floor (low + (high-low)/2)
+    let currVal = arr[mid]
+    if(currVal<target){
+      low = mid + 1
+    }
+    else if(currVal == target){
+      return mid
+    }
+    else if(currVal>target){
+      high = mid - 1
+    }
+  }
+  return -1
+}
--- 7. Debounce/index.js
+++ 7. Debounce/index.js
@@ -1,1 +1,16 @@
 // This is the index.js file for 7. Debounce
+/**
+ * @callback func
+ * @param {number} wait
+ * @return {Function}
+ */
+export default function debounce(func, wait) {
+    let timeoutId
+    return function(...args){
+      // const context = this
+      clearTimeout(timeoutId)
+      timeoutId = setTimeout(()=>{
+        func.apply(this, args)
+      },wait)
+    }
+  }
